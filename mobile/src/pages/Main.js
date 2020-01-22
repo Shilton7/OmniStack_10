17c9a79893aa16e50fd417ff5101b9e1d87ad4cc
Shilton <shilton.sistemas@gmail.com>
2020-01-22 11:39:25 -0300

    Mobile - Installation and configuration MapView

--- mobile/src/pages/Main.js
+++ mobile/src/pages/Main.js
@@ -1,8 +1,45 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet } from 'react-native';
+import MapView from 'react-native-maps';
+import { requestPermissionsAsync, getCurrentPositionAsync } from 'expo-location';
 
 function Main() {
-  return <View></View>;
+  const [localizacaoAtual, setLocalizacaoAtual] = useState(null);
+
+  useEffect(() => {
+    async function carregaPosicaoInicial() {
+      //permissão
+      const { granted } = await requestPermissionsAsync();
+
+      //recuperar localização atual
+      if (granted) {
+        const { coords } = await getCurrentPositionAsync({
+          enableHighAccuracy: true //gps habilitado
+        });
+
+        const { latitude, longitude } = coords;
+        setLocalizacaoAtual({
+          latitude,
+          latitudeDelta: 0.04, //zoom maps
+          longitude,
+          longitudeDelta: 0.04 //zoom maps
+        });
+      }
+    }
+    carregaPosicaoInicial();
+  }, []);
+
+  if (!localizacaoAtual) {
+    return null;
+  }
+
+  return <MapView initialRegion={localizacaoAtual} style={syles.map} />;
 }
+
+const syles = StyleSheet.create({
+  map: {
+    flex: 1
+  }
+});
 
 export default Main;
